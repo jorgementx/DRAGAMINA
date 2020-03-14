b23f1e3a6af20a2cf867e32e7aa4c68250d1b43d
jordi <jordi@LAPTOP-F0CU8PDM>
2020-03-14 17:38:16 +0100

    singleton dragamina

--- Dragamina/src/packDragamina/Dragamina.java
+++ Dragamina/src/packDragamina/Dragamina.java
@@ -1,5 +1,18 @@
 package packDragamina;
 
-public class Dragamina {
-
+public class Dragamina {//singleton
+	
+	private static Dragamina nDragamina=null;
+	
+	private Dragamina(){	
+		
+	}
+	
+	public static Dragamina getNireDragamina(){
+		 if (nDragamina==null){
+			 nDragamina= new Dragamina();
+		 }
+		 return nDragamina;
+	}
+	
 }
