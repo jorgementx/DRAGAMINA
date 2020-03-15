8e339752c6493fa987b025bbdac5fde7612235ac
josuf <josuf@10.0.3.11>
2020-03-15 09:59:37 +0100

    Taula ez da JFrame nire ustez

--- Dragamina/src/packDragamina/Taula.java
+++ Dragamina/src/packDragamina/Taula.java
@@ -1,53 +1,14 @@
 package packDragamina;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-public class Taula extends JFrame {
-
-	private JPanel contentPane;
-	private JPanel panel;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Taula frame = new Taula();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
+public class Taula {
+	
+	private Gelaxka[][] gelaxkaMatrizea;
+	
 	public Taula() {
-		initialize();
-	}
-	private void initialize() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-		contentPane.add(getPanel(), BorderLayout.NORTH);
+		
 	}
 
-	private JPanel getPanel() {
-		if (panel == null) {
-			panel = new JPanel();
-		}
-		return panel;
+	public void taulaHasieratu(int pZutab, int pErrenk) {
+		this.gelaxkaMatrizea=new Gelaxka[pZutab][pErrenk];
 	}
 }
