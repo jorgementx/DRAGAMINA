d2c34287cfd0cf3cca4e8029ce9ba026c543d6a6
josuf <josuf@10.0.3.11>
2020-03-15 08:47:09 +0100

    Dragamina JFrame izatera pasatu

--- Dragamina/src/packDragamina/Dragamina.java
+++ Dragamina/src/packDragamina/Dragamina.java
@@ -1,27 +1,50 @@
-package packDragamina;//jdv
+package packDragamina;
 
-public class Dragamina {
-	
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Dragamina extends JFrame {
+
+	private JPanel contentPane;
 	private static Dragamina nDragamina=null;
-	private Taula taula;
-	
-	private Dragamina(){	
-		this.taula=new Taula();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Dragamina frame = Dragamina.getNireDragamina();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	private Dragamina() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 	}
 	
-	public static Dragamina getNireDragamina(){
-		 if (nDragamina==null){
-			 nDragamina= new Dragamina();
-		 }
-		 return nDragamina;
+	public static Dragamina getNireDragamina() {
+		if (nDragamina==null) {
+			nDragamina=new Dragamina();
+		}
+		return nDragamina;
 	}
-	
-	public static void main(String [ ] args) {
-		Dragamina.getNireDragamina().jokoaHasieratu();
-	}
-	
-	private void jokoaHasieratu() {
-		this.taula.taulaHasieratu();
-	}
-	
+
 }
