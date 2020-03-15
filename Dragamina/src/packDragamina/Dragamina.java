98d2ce73c85746dbaafba38aec380b43cb1e635c
josuf <josuf@10.0.3.11>
2020-03-15 11:08:25 +0100

    Matrizearen hasieraketa

--- Dragamina/src/packDragamina/Dragamina.java
+++ Dragamina/src/packDragamina/Dragamina.java
@@ -2,15 +2,21 @@ package packDragamina;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 public class Dragamina extends JFrame {
 
+	private Taula nireTaula;
+	private int zutab;
+	private int errenk;
 	private JPanel contentPane;
-	private static Dragamina nDragamina=null;
+	private JPanel panelGelaxkak;
 
 	/**
 	 * Launch the application.
@@ -19,7 +25,7 @@ public class Dragamina extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Dragamina frame = Dragamina.getNireDragamina();
+					Dragamina frame = new Dragamina();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -31,20 +37,44 @@ public class Dragamina extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	private Dragamina() {
+	public Dragamina() {
+		this.nireTaula=new Taula(2);
+		this.zutab=this.nireTaula.getZutab();
+		this.errenk=this.nireTaula.getErrenk();
+		initialize();
+	}
+	private void initialize() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.add(getPanelGelaxkak(), BorderLayout.NORTH);
+		this.gelaxkenMatrizeaHasieratu();
 	}
 	
-	public static Dragamina getNireDragamina() {
-		if (nDragamina==null) {
-			nDragamina=new Dragamina();
+	private void gelaxkenMatrizeaHasieratu() {
+		this.nireTaula.taulaHasieratu();
+		Gelaxka pGelaxka=null;
+		for (int i=0;i<this.errenk;i++) {
+			for (int j=0;j<this.zutab;j++) {
+				if (this.nireTaula.getGelaxka(i, j) instanceof Mina) {
+					pGelaxka=new Mina();
+				}
+				else if (this.nireTaula.getGelaxka(i, j) instanceof Hutsik) {
+					pGelaxka=new Hutsik();
+				}
+				this.getPanelGelaxkak().add(pGelaxka);
+				this.getPanelGelaxkak().add(pGelaxka, new GridBagConstraints(i,j,1,1,0.0,0.0,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(0, 0, 0, 0),0,0));
+			}
 		}
-		return nDragamina;
 	}
-
+	private JPanel getPanelGelaxkak() {
+		if (panelGelaxkak == null) {
+			panelGelaxkak = new JPanel();
+			panelGelaxkak.setLayout(new GridBagLayout());
+		}
+		return panelGelaxkak;
+	}
 }
--- Dragamina/src/packDragamina/Taula.java
+++ Dragamina/src/packDragamina/Taula.java
@@ -3,12 +3,51 @@ package packDragamina;
 public class Taula {
 	
 	private Gelaxka[][] gelaxkaMatrizea;
+	private int minaKop;
 	
-	public Taula() {
-		
+	public Taula(int zailtasuna) {
+		if (zailtasuna==1) {
+			this.gelaxkaMatrizea=new Gelaxka[7][10];
+		}
+		else if (zailtasuna==2) {
+			this.gelaxkaMatrizea=new Gelaxka[10][15];
+		}
+		else if (zailtasuna==3) {
+			this.gelaxkaMatrizea=new Gelaxka[12][25];
+		}
+		this.minaKop=this.gelaxkaMatrizea.length*zailtasuna;
 	}
 
-	public void taulaHasieratu(int pZutab, int pErrenk) {
-		this.gelaxkaMatrizea=new Gelaxka[pZutab][pErrenk];
+	public void taulaHasieratu() {
+		int jartzekoFalta=this.minaKop;
+		int randomZut=0;
+		int randomErrenk=0;
+		while (jartzekoFalta>0) {
+			randomErrenk=(int)(Math.random()*this.gelaxkaMatrizea.length);
+			randomZut=(int)(Math.random()*this.gelaxkaMatrizea[0].length);
+			if (this.gelaxkaMatrizea[randomErrenk][randomZut]==null) {
+				this.gelaxkaMatrizea[randomErrenk][randomZut]=new Mina();
+				jartzekoFalta--;
+			}
+		}
+		for (int i=0;i<this.gelaxkaMatrizea.length;i++) {
+			for (int j=0;j<this.gelaxkaMatrizea[0].length;j++) {
+				if (this.gelaxkaMatrizea[i][j]==null) {
+					this.gelaxkaMatrizea[i][j]=new Hutsik();
+				}
+			}
+		}
+	}
+	
+	public int getErrenk() {
+		return this.gelaxkaMatrizea.length;
+	}
+	
+	public int getZutab() {
+		return this.gelaxkaMatrizea[0].length;
+	}
+	
+	public Gelaxka getGelaxka(int pZutab, int pErrenk) {
+		return this.gelaxkaMatrizea[pZutab][pErrenk];
 	}
 }
