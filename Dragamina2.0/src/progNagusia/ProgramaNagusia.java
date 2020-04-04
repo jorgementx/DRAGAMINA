a4d96d5f63daf7331555265f2ff31b03b365137b
Goizane <goizanealonso95@gmail.com>
2020-04-04 22:53:41 +0200

    Froga

--- Dragamina2.0/src/progNagusia/ProgramaNagusia.java
+++ Dragamina2.0/src/progNagusia/ProgramaNagusia.java
@@ -2,6 +2,7 @@ package progNagusia;
 
 import Bista.Dragamina;
 
+// Programa nagusia
 public class ProgramaNagusia {
 	public static void main(String[] args) {
 		Dragamina nD = Dragamina.getDragamina();
